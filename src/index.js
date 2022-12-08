0d36e17a0541aab7dd8861283f18068681473277
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-08 07:18:18 +0200

    remove all stylelinter and elint erros

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import './style.css';
-import DomToDo from './modules/domDisplay.js';
+import DomToDo from './modules/display.js';
 import CreateToDo from './modules/constructor.js';
 import Store from './modules/store.js';
+import setTime from './modules/dates.js';
 
+setTime();
 document.addEventListener('DOMContentLoaded', DomToDo.displayToDo);
 document.getElementById('completedBtn').addEventListener('click', () => {
-  Storage.removeCompleted();
+  Store.removeCompleted();
 });
 
 document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const todoL = Storage.getToDo();
+  const todoL = Store.getToDo();
   const toDoInput = document.getElementById('todo-input').value;
   const id = todoL.length + 1;
   const completed = false;
   const todo = new CreateToDo(toDoInput, id, completed);
   DomToDo.addToDoList(todo);
-  Storage.addTodo(todo);
+  Store.addTodo(todo);
   DomToDo.clearField();
 });
 document.getElementById('to-do-container').addEventListener('click', (e) => {
-  Storage.editInput(
+  Store.editInput(
     e.target.parentElement.parentElement.children[4].textContent,
     e.target.parentElement,
     e.target.parentElement.parentElement,
@@ -28,7 +30,7 @@ document.getElementById('to-do-container').addEventListener('click', (e) => {
   );
   DomToDo.deleteTodo(e.target);
   if (e.target.classList.contains('check')) {
-    Storage.checkboxCompleted(
+    Store.checkboxCompleted(
       e.target.parentElement.parentElement.children[4],
       e.target.checked,
     );
@@ -36,7 +38,7 @@ document.getElementById('to-do-container').addEventListener('click', (e) => {
       'strike-through',
     );
   }
-  Storage.remove(
+  Store.remove(
     e.target.parentElement.previousElementSibling.previousElementSibling
       .textContent,
   );

afa08a58358251a1e9a5592cfe99b1b6cee382bd
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-08 05:57:54 +0200

    Create constructor file

---
+++ src/modules/constructor.js
@@ -0,0 +1,10 @@
+class CreateToDo {
+    constructor(description, id, completed) {
+      this.description = description;
+      this.id = id;
+      this.completed = completed;
+    }
+  }
+  
+  export default CreateToDo;
+  
